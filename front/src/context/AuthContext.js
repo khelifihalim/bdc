03f3bece2b4e33a9ca7251c6cf22b5b18095ec73
khelifihalim <105592665+khelifihalim@users.noreply.github.com>
2024-09-29 17:54:34 +0100

    Update AuthContext.js

--- front/src/context/AuthContext.js
+++ front/src/context/AuthContext.js
@@ -5,6 +5,8 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState(null);
 
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
